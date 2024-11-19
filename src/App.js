f0f99063b9a84b7dd3efaa9b044cf6f009c180e2
Nicolás Segura <nseg97@gmail.com>
2024-11-19 00:11:46 -0300

    try

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <div className="container-fluid">
           <Routes>
             <Route path="/app1" element={<App1 />} />
-            <Route path="/app2" element={<App2 />} />
+            <Route path="/app2" element={<App1 />} />
             <Route path="/app3" element={<App3 />} />
             <Route path="/" element={<App1 />} /> {/* Ruta por defecto */}
           </Routes>
